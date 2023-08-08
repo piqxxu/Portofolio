da0628fe2877eb8b584e150c3a5c8285972c247a
piqxxu <piquradya@gmail.com>
2023-08-08 08:35:26 +0700

    add about effect

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 function toggleDetail (e) {
     const target = $(e.target)
-    if ($(target).hasClass("active")) {
-            $(target).html("More Info").removeClass("active")
-        }
+
+    if($(target).hasClass('active')) {
+        $(target).html('More Info').removeClass('active')
+    }
     else {
-        $(target.html("Less Info")).addClass ("active")
+        $(target).html('Less Info').addClass('active')
     }
 
-    const item = $(target).parents(".about-exp-item") 
-    const detail = $(item).children(".about-exp-item-detail")
-
-    $(detail.slideToggle())
+    const parent = $(target).parents(".about-exp-item")
+    const detail = $(parent).children(".about-exp-item-detail")
+    
+    $(detail).slideToggle()
 }
